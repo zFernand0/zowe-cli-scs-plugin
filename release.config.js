80f5e7f2ce56e0c4d957062d4d805417ae2409f5
zFernand0 <fernando.rijocedeno@broadcom.com>
2022-10-03 17:26:24 +0000

    [test] attempt to deploy to GH releases (for now)
    Signed-off-by: zFernand0 <fernando.rijocedeno@broadcom.com>

--- release.config.js
+++ release.config.js
@@ -1,22 +1,31 @@
 module.exports = {
-  branches: [
-      {
-          name: "master",
-          level: "patch",
-          devDependencies: ["@zowe/imperative"]
-      }
-  ],
-  plugins: [
-      "@octorelease/changelog",
-      ["@octorelease/npm", {
-          aliasTags: {
-              latest: ["zowe-v1-lts"]
-          },
-          pruneShrinkwrap: true
-      }],
-      ["@octorelease/github", {
-          checkPrLabels: true
-      }],
-      "@octorelease/git"
-  ]
+    branches: [
+        {
+            name: "master",
+            level: "patch",
+            devDependencies: ["@zowe/imperative"]
+        },
+        {
+            name: "update-workflows",
+            level: "patch",
+            prerelease: true,
+            devDependencies: ["@zowe/imperative"]
+        }
+    ],
+    plugins: [
+        "@octorelease/changelog",
+        ["@octorelease/npm", {
+            npmPublish: false,
+            tarballDir: "dist",
+            aliasTags: {
+                latest: ["zowe-v1-lts"]
+            },
+            pruneShrinkwrap: true
+        }],
+        ["@octorelease/github", {
+            assets: "dist/*.tgz",
+            checkPrLabels: true
+        }],
+        "@octorelease/git"
+    ]
 };
