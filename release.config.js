1fc6d62a148401536e8aa20df29177194060d565
zFernand0 <fernando.rijocedeno@broadcom.com>
2022-10-03 17:47:38 +0000

    update release config again
    Signed-off-by: zFernand0 <fernando.rijocedeno@broadcom.com>

--- release.config.js
+++ release.config.js
@@ -9,9 +9,8 @@ module.exports = {
             name: "update-workflows",
             level: "patch",
             prerelease: true,
-            devDependencies: {
-                "@zowe/imperative": "zowe-v1-lts"
-            }
+            devDependencies: ["@zowe/imperative"],
+            channel: "zowe-v1-lts"
         }
     ],
     plugins: [
